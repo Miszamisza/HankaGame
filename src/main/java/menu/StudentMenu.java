5b2a57cdc523681c9402317f5c6e42185ce60ce1
Misha <zielinskanoemi5@gmail.com>
2019-09-16 10:39:54 +0200

    Faktory i menu. Ogólne podwaliny

--- src/main/java/menu/StudentMenu.java
+++ src/main/java/menu/StudentMenu.java
@@ -1,7 +1,6 @@
 package menu;
 
-import javafx.geometry.HorizontalDirection;
-import javafx.scene.input.ScrollEvent;
+
 import studentFactory.StudentFactory;
 
 import java.awt.*;
@@ -18,13 +17,14 @@ public class StudentMenu extends JFrame implements ActionListener{
 
     public static final int HEIGHT = 720;
     public static final int WIDTH = 1280;
+    private JTextArea JTextArea = new JTextArea();
     private JButton fanPiwa;
     private JButton fanWodki;
     private JButton fanWina;
     private JButton narkoMan;
 
     public StudentMenu() {
-        fanPiwa = new JButton("Fan piwa");
+        fanPiwa = new JButton("Fan piwa"); fanPiwa.setPreferredSize(new Dimension(100,100));
         fanWodki = new JButton("Fan wódki");
         fanWina = new JButton("Fan Wina");
         narkoMan = new JButton("NarkoMan");
@@ -39,6 +39,7 @@ public class StudentMenu extends JFrame implements ActionListener{
         add(fanWodki);
         add(fanWina);
         add(narkoMan);
+        add(JTextArea);
     }
 
     @Override
@@ -46,18 +47,7 @@ public class StudentMenu extends JFrame implements ActionListener{
         Object source = e.getSource();
 
         StudentFactory studentFactory = new StudentFactory();
-        if(source == fanPiwa)
-            studentFactory.studentCreate(fanPiwa);
-
-        else if(source == fanWodki)
-            studentFactory.studentCreate(fanWodki);
-
-        else if(source == fanWina)
-            studentFactory.studentCreate(fanWina);
-
-        else if(source == narkoMan)
-            studentFactory.studentCreate(narkoMan);
-
+        studentFactory.studentCreate(source.toString());
     }
 }
 
