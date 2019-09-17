7db96b4bce722bb210b46f3c3d46c74e760519b3
Adrian <zielinskanoemi5@gmail.com>
2019-09-17 02:11:07 +0200

    Game Window + F:PS  READY

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -21,13 +21,98 @@ public class GamePanel extends JPanel implements Runnable {
 
 
     public GamePanel(int width, int height) {
+        this.width = width;
+        this.height = height;
         setPreferredSize(new Dimension(width, height));
         setFocusable(true);
         requestFocus();
     }
 
-    public void run() {
+    public  void addNotify() {
+        super.addNotify();
 
+        if (thread == null) {
+            thread = new Thread(this, "HankaGameThread");
+            thread.start();
+        }
+    }
+
+    public void run() {
+        init();
+        final double GAME_HERTZ = 60.0;
+        final double TBU = 1000000000/GAME_HERTZ;
+
+        final  int MUBR =5;
+
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+
+        final double TARGET_FPS=60;
+        final  double TTBR = 1000000000 / TARGET_FPS;
+
+        int frameCaunt = 0;
+        int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+        int oldFrameCount = 0;
+
+        while (running){
+            double now =System.nanoTime();
+            int updateCount = 0;
+            while(((now - lastUpdateTime) > TBU) && (updateCount <MUBR)) {
+                update();
+                input();
+                lastUpdateTime += TBU;
+                updateCount++;
+            }
+            if (now - lastUpdateTime > TBU) {
+                lastUpdateTime = now - TBU;
+            }
+            input();
+            render();
+            draw();
+            lastRenderTime = now;
+            frameCaunt++;
+
+            int thisSecond = (int) (lastUpdateTime / 1000000000);
+            if (thisSecond > lastSecondTime) {
+                if (frameCaunt != oldFrameCount){
+                    System.out.println("NEW SECOND " + thisSecond + "  " +frameCaunt);
+                    oldFrameCount = frameCaunt;
+                }
+                frameCaunt = 0;
+                lastSecondTime = thisSecond;
+            }
+            while (now - lastRenderTime < TTBR && now - lastUpdateTime < TBU) {
+                Thread.yield();
+
+                try {
+                    Thread.sleep(1);
+                }catch(Exception e){
+                    System.out.println("ERROR: Yielding thread");
+                }
+                now = System.nanoTime();
+
+            }
+        }
+    }
+    private void input(){
+
+    }
+
+    private void update() {
+
+    }
+    public void render(){
+
+    }
+    public void draw(){
+
+    }
+
+    public void init() {
+        running = true;
+
+        img = new BufferedImage(width, height, TYPE_INT_ARGB);
+        g= (Graphics2D) img.getGraphics();
     }
 }
 
--- src/main/java/GameWindow.java
+++ src/main/java/GameWindow.java
@@ -10,4 +10,6 @@ public class GameWindow extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
     }
+
+
 }
